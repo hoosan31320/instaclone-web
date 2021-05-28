46c1891b69df8e852f8d347b2057261b993cc7f5
hoosan31320 <hoos31320@gmail.com>
2021-05-28 23:32:18 +0700

    9.2 Cleaning Login Code

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,6 @@
-import {
-    faFacebook,
-    faFacebookF,
-    faFacebookSquare,
-    faInstagram
-} from "@fortawesome/free-brands-svg-icons";
+import { faFacebookSquare, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -17,7 +13,7 @@ const Container = styled.div`
 
 const WhiteBox = styled.div`
     background-color: white;
-    border: 1px solid rgb(219, 219, 219);
+    border: 1px solid ${(props) => props.theme.borderColor};
     width: 100%;
 `;
 
@@ -35,28 +31,32 @@ const TopBox = styled(WhiteBox)`
         justify-items: center;
         flex-direction: column;
         align-items: center;
-        input{
-            width: 100%;
-            border-radius: 3px;
-            padding: 7px;
-            background-color: #fafafa;
-            border: 0.5px solid rgb(219, 219, 219);
-            margin-top: 5px;
-            box-sizing: border-box;
-            &::placeholder {
-                font-size: 12px;
-            }
-            &::last-child {
-                border: none;
-                margin-top: 12px;
-                background-color: #0095f6;
-                color: white;
-                text-align: center;
-                padding: 8px 0px;
-                font-weight: 600;
-            }
-        }
     }
+`;
+
+const Input = styled.input`
+    width: 100%;
+    border-radius: 3px;
+    padding: 7px;
+    background-color: #fafafa;
+    border: 0.5px solid ${(props) => props.theme.borderColor};
+    margin-top: 5px;
+    box-sizing: border-box;
+    &::placeholder {
+        font-size: 12px;
+    }
+`;
+
+const Button = styled.input`
+    border: none;
+    border-radius: 3px;
+    margin-top: 12px;
+    background-color: ${(props) => props.theme.accent};
+    color: white;
+    text-align: center;
+    padding: 8px 0px;
+    font-weight: 600;
+    width: 100%;
 `;
 
 const BottomBox = styled(WhiteBox)`
@@ -64,7 +64,8 @@ const BottomBox = styled(WhiteBox)`
     text-align: center;
     a {
     font-weight: 600;
-    color: #0095f6;
+    margin-left: 5px;
+    color: ${(props) => props.theme.accent}
     }
 `;
 
@@ -83,11 +84,12 @@ const Separator = styled.div`
     div {
     width: 100%;
     height: 1px;
-    background-color: rgb(219, 219, 219);
+    background-color: ${(props) => props.theme.borderColor};
     }
     span {
     margin: 0px 10px;
     font-weight: 600;
+    font-size: 12px;
     color: #8e8e8e;
     }
 `;
@@ -100,10 +102,6 @@ const FacebookLogin = styled.div`
     }
 `;
 
-// const Title = styled.h1`
-//     color: ${(props) => props.theme.fontColor};
-// `;
-
 function Login() {
     return (
         <Container>
@@ -113,9 +111,9 @@ function Login() {
                         <FontAwesomeIcon icon={faInstagram} size="3x" />
                     </div>
                     <form>
-                        <input type="text" placeholder="Username" />
-                        <input type="password" placeholder="Password" />
-                        <input type="submit" value="Log in" />
+                        <Input type="text" placeholder="Username" />
+                        <Input type="password" placeholder="Password" />
+                        <Button type="submit" value="Log in" />
                     </form>
                     <Separator>
                         <div></div>
@@ -128,7 +126,8 @@ function Login() {
                     </FacebookLogin>
                 </TopBox>
                 <BottomBox>
-                    <span>Don't have an account?</span> <a href="www.google.com">Sign Up</a>
+                    <span>Don't have an account?</span> 
+                    <Link to="/sign-up">Sign Up</Link>
                 </BottomBox>
             </Wrapper>
         </Container>
