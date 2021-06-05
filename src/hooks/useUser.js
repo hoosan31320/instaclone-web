dfc682557fcf7b778cb56a49ee6ca81a0c2926ca
hoosan31320 <hoos31320@gmail.com>
2021-06-05 20:24:56 +0700

    10.1 Header part Two

---
+++ src/hooks/useUser.js
@@ -0,0 +1,20 @@
+import { gql, useQuery, useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "../apollo";
+
+const ME_QUERY = gql`
+  query me {
+    me {
+      username
+      avatar
+    }
+  }
+`;
+
+function useUser() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const { data, error } = useQuery(ME_QUERY, {
+    skip: !isLoggedIn,
+  });
+  return;
+}
+export default useUser;
