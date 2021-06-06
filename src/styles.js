36a55ef41c1605b7a1023da006be8c4f26d51937
hoosan31320 <hoos31320@gmail.com>
2021-06-07 00:09:35 +0700

    Auto Deploy Checked

--- src/styles.js
+++ src/styles.js
@@ -18,7 +18,7 @@ export const GlobalStyles = createGlobalStyle`
     input { all: unset; }
     * { box-sizing: border-box; }
     body {
-        background-color: red;
+        background-color: ${(props) => props.theme.bgColor};
         font-size: 14px;
         font-family: 'Open Sans', sans-serif;
         color: ${(props) => props.theme.fontColor};
