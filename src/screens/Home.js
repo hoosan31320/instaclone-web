1ef3a2374d1a82e80f2e4d6e340cee7699daf2cf
hoosan31320 <hoos31320@gmail.com>
2021-06-10 19:52:17 +0700

    seeFeed offset setting

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,8 +5,8 @@ import PageTitle from "../components/PageTitle";
 import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
 
 const FEED_QUERY = gql`
-  query seeFeed {
-    seeFeed(offset: 0) {
+  query seeFeed($offset: Int!) {
+    seeFeed(offset: $offset) {
       ...PhotoFragment
       user {
         username
@@ -24,7 +24,9 @@ const FEED_QUERY = gql`
 `;
 
 function Home() {
-    const { data } = useQuery(FEED_QUERY);
+    const { data } = useQuery(FEED_QUERY, {
+      variables: { offset: 0 }
+    });
     return (
         <div>
             <PageTitle title="Home" />
